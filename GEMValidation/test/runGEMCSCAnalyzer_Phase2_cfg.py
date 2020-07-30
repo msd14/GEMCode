da6fdb4b1306a18d7355a2bd3990cdd4068c19b5
Sven Dildick <sven.dildick@cern.ch>
2020-07-30 15:15:07 -0500

    Phase 2 configuration

---
+++ GEMValidation/test/runGEMCSCAnalyzer_Phase2_cfg.py
@@ -0,0 +1,102 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Era_Phase2C10_cff import Phase2C10
+
+process = cms.Process('ANA',Phase2C10)
+
+## Standard sequence
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi')
+process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAlong_cfi')
+
+"""
+process.MessageLogger = cms.Service("MessageLogger",
+    destinations = cms.untracked.vstring("debug"),
+    debug = cms.untracked.PSet(
+ #       extension = cms.untracked.string(".txt"),
+        threshold = cms.untracked.string("DEBUG"),
+        # threshold = cms.untracked.string("WARNING"),
+        lineLength = cms.untracked.int32(132),
+        noLineBreaks = cms.untracked.bool(True)
+    ),
+    debugModules = cms.untracked.vstring("GEMCSCAnalyzer")
+)
+"""
+process.source = cms.Source(
+  "PoolSource",
+  fileNames = cms.untracked.vstring('file:step2bis.root'),
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-100) )
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("out_ana_phase2.root")
+)
+
+## global tag for upgrade studies
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T15', '')
+
+# the analyzer configuration
+from GEMCode.GEMValidation.simTrackMatching_cfi import simTrackPSet
+process.GEMCSCAnalyzer = cms.EDAnalyzer(
+    "GEMCSCAnalyzer",
+    simTrackPSet,
+    runSim = cms.bool(True),
+    runDigi = cms.bool(True),
+    runStub = cms.bool(True),
+    runL1 = cms.bool(True),
+    runReco = cms.bool(False),
+    verbose = cms.untracked.int32(1),
+    minNHitsChamberGEMSimHit = cms.int32(1),
+    minNHitsChamberCSCSimHit = cms.int32(4),
+    minNHitsChamberCSCDigi = cms.int32(4),
+    minNHitsChamberGEMDigi = cms.int32(1),
+    minNHitsChamberCSCStub = cms.int32(4),
+)
+
+ana = process.GEMCSCAnalyzer
+ana.simTrack.minEta = 0.9
+ana.simTrack.maxEta = 2.4
+ana.simTrack.minPt = 3
+ana.gemSimHit.verbose = 0
+ana.gemStripDigi.verbose = 0
+ana.gemStripDigi.matchDeltaStrip = 2
+ana.gemPadDigi.verbose = 0
+ana.gemCoPadDigi.verbose = 0
+ana.gemPadCluster.verbose = 0
+ana.cscComparatorDigi.verbose = 0
+ana.cscWireDigi.verbose = 0
+ana.cscALCT.verbose = 0
+ana.cscCLCT.verbose = 0
+ana.cscLCT.verbose = 0
+
+ana.cscALCT.inputTag = cms.InputTag("simCscTriggerPrimitiveDigis","","ReL1")
+ana.cscCLCT.inputTag = cms.InputTag("simCscTriggerPrimitiveDigis","","ReL1")
+ana.cscLCT.inputTag = cms.InputTag("simCscTriggerPrimitiveDigis","","ReL1")
+ana.cscMPLCT.inputTag = cms.InputTag("simCscTriggerPrimitiveDigis","MPCSORTED","ReL1")
+
+useUnpacked = False
+if useUnpacked:
+    ana.gemStripDigi.inputTag = "muonGEMDigis"
+    ana.muon.inputTag = cms.InputTag("gmtStage2Digis","Muon")
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.p = cms.Path(process.GEMCSCAnalyzer)
+
+## messages
+print
+#print 'Input files:'
+print '----------------------------------------'
+print process.source.fileNames
+print
+print 'Output file:'
+print '----------------------------------------'
+print process.TFileService.fileName
+print
