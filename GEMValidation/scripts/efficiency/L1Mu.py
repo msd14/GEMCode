687eccdf05eb38b63a67e642d5c3e0a41785b0e1
Sven Dildick <sven.dildick@cern.ch>
2020-07-31 03:37:59 -0500

    more emtf plots

--- GEMValidation/scripts/efficiency/L1Mu.py
+++ GEMValidation/scripts/efficiency/L1Mu.py
@@ -99,6 +99,89 @@ def EMTFEta(plotter):
     del c, base, h1, leg
 
 
+def EMTFEta2(plotter):
+
+    ## variables for the plot
+    topTitle = ""
+    xTitle = "True muon |#eta|"
+    yTitle = "Efficiency"
+    title = "%s;%s;%s"%(topTitle,xTitle,yTitle)
+    toPlot = "eta"
+    subdirectory = "efficiency/L1Mu/"
+
+    h_bins = "(50,-2.4,2.4)"
+    nBins = int(h_bins[1:-1].split(',')[0])
+    minBin = float(h_bins[1:-1].split(',')[1])
+    maxBin = float(h_bins[1:-1].split(',')[2])
+
+    c = TCanvas("c","c",800,600)
+    c.Clear()
+    base  = TH1F("base",title,nBins,minBin,maxBin)
+    base.SetMinimum(0)
+    base.SetMaximum(plotter.yMax)
+    base.Draw("")
+    base.GetXaxis().SetLabelSize(0.05)
+    base.GetYaxis().SetLabelSize(0.05)
+    base.GetXaxis().SetTitleSize(0.05)
+    base.GetYaxis().SetTitleSize(0.05)
+
+    h1 = draw_geff(plotter.tree, title, h_bins, toPlot, ok_eta(0.9, 2.4), ok_emtf(20), "same", kBlue)
+
+    leg = TLegend(0.45,0.2,.75,0.5, "", "brNDC");
+    leg.SetBorderSize(0)
+    leg.SetFillStyle(0)
+    leg.SetTextSize(0.06)
+    leg.AddEntry(h1, "EMTF, p_{T} > 20 GeV","l")
+    leg.Draw("same");
+
+    c.Print("%sEff_EMTF2_eta_Pt20_%s"%(plotter.targetDir + subdirectory, plotter.ext))
+
+    del c, base, h1, leg
+
+
+def EMTFPhi(plotter):
+
+    ## variables for the plot
+    topTitle = ""
+    xTitle = "True muon #phi"
+    yTitle = "Efficiency"
+    title = "%s;%s;%s"%(topTitle,xTitle,yTitle)
+    toPlot = "phi"
+    subdirectory = "efficiency/L1Mu/"
+
+    h_bins = "(64,-3.2,3.2)"
+    nBins = int(h_bins[1:-1].split(',')[0])
+    minBin = float(h_bins[1:-1].split(',')[1])
+    maxBin = float(h_bins[1:-1].split(',')[2])
+
+    c = TCanvas("c","c",800,600)
+    c.Clear()
+    base  = TH1F("base",title,nBins,minBin,maxBin)
+    base.SetMinimum(0)
+    base.SetMaximum(plotter.yMax)
+    base.Draw("")
+    base.GetXaxis().SetLabelSize(0.05)
+    base.GetYaxis().SetLabelSize(0.05)
+    base.GetXaxis().SetTitleSize(0.05)
+    base.GetYaxis().SetTitleSize(0.05)
+
+    h1 = draw_geff(plotter.tree, title, h_bins, toPlot, ok_eta(0.9, 2.4), ok_emtf(20), "same", kBlue)
+
+    leg = TLegend(0.45,0.2,.75,0.5, "", "brNDC");
+    leg.SetBorderSize(0)
+    leg.SetFillStyle(0)
+    leg.SetTextSize(0.06)
+    leg.AddEntry(h1, "EMTF, p_{T} > 20 GeV","l")
+    leg.Draw("same");
+
+    c.Print("%sEff_EMTF2_phi_Pt20_%s"%(plotter.targetDir + subdirectory, plotter.ext))
+
+    del c, base, h1, leg
+
+
+
 def L1Mu(plotter):
     EMTFPt(plotter)
     EMTFEta(plotter)
+    EMTFEta2(plotter)
+    EMTFPhi(plotter)
