f274b00b72cc6710a3c05896455070bb68e41ade
Sven Dildick <sven.dildick@cern.ch>
2020-08-06 21:13:25 -0500

    Update script

--- GEMValidation/scripts/datavsemulator/comparison_v5.py
+++ GEMValidation/scripts/datavsemulator/comparison_v5.py
@@ -5,21 +5,20 @@ import ROOT
 ROOT.gROOT.SetBatch(1)
 #ROOT.gErrorIgnoreLevel=1001
 
-cscstations = [ [0,0], 
-                [1,1], [1,2], [1,3], [1,4],
+cscstations = [ [0,0],
+                [1,1], [1,2], [1,3],
                 [2,1], [2,2],
                 [3,1], [3,2],
                 [4,1], [4,2],]
 csclabel = {
     1 : {
         0 : {
-            0 : ["pAll", 'ME+'] 
+            0 : ["pAll", 'ME+']
             },
         1 : {
-            1 : ["pME1b","ME+1/1b"],
+            1 : ["pME11","ME+1/1"],
             2 : ["pME12","ME+1/2"],
-            3 : ["pME13","ME+1/3"],
-            4 : ["pME1a","ME+1/1a"]
+            3 : ["pME13","ME+1/3"]
             },
         2 : {
             1 : ["pME21","ME+2/1"],
@@ -39,10 +38,9 @@ csclabel = {
             0 : ["mAll","ME-"]
             },
         1 : {
-            1 : ["mME1b","ME-1/1b"],
+            1 : ["mME11","ME-1/1"],
             2 : ["mME12","ME-1/2"],
-            3 : ["mME13","ME-1/3"],
-            4 : ["mME1a","ME-1/1a"],
+            3 : ["mME13","ME-1/3"]
             },
         2 : {
             1 : ["mME21","ME-2/1"],
@@ -60,41 +58,16 @@ csclabel = {
     }
 gROOT.SetBatch(1)
 gStyle.SetStatStyle(0)
-gStyle.SetOptStat("nemr")
+gStyle.SetOptStat("nemrou")
 
 ch = TChain("Events")
 fc = TFileCollection("dum")
-#fc.Add("/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v2/160811_011552/0000/*.root")
-fc.Add("SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_backup.root")
-#SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_backup.root
-print fc
+fc.Add("step2bis_pu200.root")
+#"/eos/uscms/store/user/dildick/RelValSingleMuFlatPt2To100/L1CSC_SingleMuPU200_11_1_X_v8_copadbx_revertclct_3layerclct/200731_202710/0000/*.root")
 fc.Print()
 ch.AddFileInfoList(fc.GetList())
-#exit(1)
-#file = TFile("SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_backup.root")
-outputdirectory = "2016_mc_SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_20160810/"
-"""
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_1.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_10.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_11.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_12.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_13.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_14.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_15.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_16.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_17.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_18.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_19.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_2.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_20.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_3.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_4.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_5.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_6.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_7.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_8.root
-/eos/uscms/store/user/lpcgem/SingleMuonPt100_MC/SingleMuonPt100_MC_L1REPACK_DIGI2RAW_v1/160810_231409/0000/SingleMuPt100_cfi_L1REPACK_RAW2DIGI_10k_Baseline_9.root
-"""
+#file = TFile("step2bis_pu200.root")
+outputdirectory = "LCT_comparison_RelValSingleMuFlatPt2To100_PU200_CMSSW_11_2_X/"
 tree = ch#.Get("Events")
 
 csccorrelatedlctdigi = {
@@ -108,33 +81,29 @@ csccorrelatedlctdigi = {
     7 : ["bx", "bx",16,0,16],
     8 : ["mpclink", "mpclink",5,0,5],
     9 : ["bx0", "bx0",10,0,10],
-    10 : ["syncErr", "syncErr",10,0,10],
-    11 : ["cscID", "cscID",15,0,15],
+    10 : ["cscID", "cscID",15,0,15],
+    11 : ["type_", "type",8,0,8],
     }
 
 cscalctdigi = {
-#    0 : ["valid_", "valid",10,0,10],
-#    1 : ["quality_", "quality",20,0,20],
-#    2 : ["accel_", "accelerator",10,0,10],
-#    3 : ["patternb_", "patternb",10,0,10],
-#    4 : ["keywire_", "keywire",150,0,150],
-    5 : ["bx_", "bx",12,0,12],
-#    6 : ["trknmb_", "trknmb",10,0,10],
-#   7 : ["fullbx_", "fullbx",10,0,10],
+    0 : ["valid_", "valid",10,0,10],
+    1 : ["quality_", "quality",20,0,20],
+    2 : ["patternb_", "patternb",10,0,10],
+    3 : ["keywire_", "keywire",150,0,150],
+    4 : ["bx_", "bx",12,0,12],
+    5 : ["trknmb_", "trknmb",10,0,10]
 }
 
 cscclctdigi = {
-#    0 : ["valid_", "valid",10,0,10],
-#    1 : ["quality_", "quality",16,0,16],
-#    2 : ["pattern_", "pattern",20,0,20],
-#    3 : ["striptype_", "striptype",10,0,10],
-#    4 : ["bend_", "bend",10,0,10],
-#    5 : ["cfeb_", "cfeb",10,0,10],
-#    6 : ["strip_", "strip",32,0,32],
+    0 : ["getKeyStrip()", "keyStrip",224,0,224],
+    1 : ["strip_", "strip",32,0,32],
+    2 : ["quality_", "quality",16,0,16],
+    3 : ["pattern_", "pattern",20,0,20],
+    4 : ["bend_", "bend",10,0,10],
+    5 : ["cfeb_", "cfeb",10,0,10],
+    6 : ["trknmb_", "trknmb",10,0,10],
     7 : ["bx_", "bx",12,0,12],
-#    8 : ["trknmb_", "trknmb",10,0,10],
-#    9 : ["fullbx_", "fullbx",10,0,10],
-#    10 : ["getKeyStrip()", "keyStrip",224,0,224],
+    8 : ["valid_", "valid",10,0,10],
     }
 
 colors = [kRed, kBlue, kBlack, kGreen+2, kOrange+2, kPink+1, 28]
@@ -149,8 +118,8 @@ def yRanges(nCollections):
         ymaxs.append(1.0-d*ydelta)
     return ymins, ymaxs
 
-    
-def compareLCTs(collections, endcap, station, ring, variable):
+
+def compareLCTs(collections, endcap, station, ring, variable, bxpar):
 
     nCollections = len(collections)
     ymins, ymaxs = yRanges(nCollections)
@@ -163,39 +132,31 @@ def compareLCTs(collections, endcap, station, ring, variable):
 
     extraCut = "strip >= 0"
     realRing = ring
-    
-    if station==1 and ring==1:
-        extraCut = "strip < 128"
-
-    if station==1 and ring==4:
-        extraCut = "strip >= 128"
-        realRing = 1
+    if bxpar != -1:
+        extraCut = "bx == 8"
 
     c = TCanvas("c","c",800,800)
     c.cd()
 
     def addCollection(collection, index):
-        collection_substring = collection[len('CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_'):] + "_" + varstr + "_" + csclabel[endcap][station][ring][0] 
-        hist = TH1D(collection_substring,"CSCCorrelatedLCTDigi " + varstr + " " + 
+        print endcap, station, ring
+        collection_substring = collection[len('CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_'):] + "_" + varstr + "_" + csclabel[endcap][station][ring][0]
+        hist = TH1D(collection_substring,"CSCCorrelatedLCTDigi " + varstr + " " +
                    csclabel[endcap][station][ring][1] + "; " + varstr + "; Entries",varnbin,varminbin,varmaxbin)
 
         if station==0 and realRing==0:
             tree.Draw(collection + ".obj.data_.second." + var + ">>" + hist.GetName(),
-                      collection + ".obj.data_.first.endcap() == %d"%(endcap))
+                      collection + ".obj.data_.first.endcap() == %d &&"%(endcap) +
+                      collection + ".obj.data_.second." + extraCut)
         else:
             tree.Draw(collection + ".obj.data_.second." + var + ">>" + hist.GetName(),
                       collection + ".obj.data_.first.endcap() == %d && "%(endcap) +
-                      collection + ".obj.data_.first.station()==%d && "%(station) + 
+                      collection + ".obj.data_.first.station()==%d && "%(station) +
                       collection + ".obj.data_.first.ring()==%d && "%(realRing) +
                       collection + ".obj.data_.second." + extraCut)
-            
+
         hist.SetLineColor(colors[index])
-        
-        
-        if varstr is 'bx':
-            if hist.GetEntries()!=0:
-                print '|', csclabel[endcap][station][ring][1], '|', hist.GetBinContent(6)/hist.GetEntries(), '|', hist.GetBinContent(7)/hist.GetEntries(), '|', hist.GetBinContent(8)/hist.GetEntries(), '|' 
-  
+
         if index==0:
             hist.Draw()
         else:
@@ -213,7 +174,7 @@ def compareLCTs(collections, endcap, station, ring, variable):
         if index==0: hist.Draw()
         else:        hist.Draw("sames")
         stats.Draw("same")
-    
+
     ## add the histograms and the stat boxes
     hists = []
     stats = []
@@ -224,15 +185,18 @@ def compareLCTs(collections, endcap, station, ring, variable):
 
     for i in range(0,nCollections):
         plotCollection(hists[i], stats[i], i)
-    
-    c.SaveAs(outputdirectory  + "comparison_lct_" + varstr + "_" + csclabel[endcap][station][ring][0] + ".png")
+
+    if bxpar != -1:
+        c.SaveAs(outputdirectory  + "comparison_lct_" + varstr + "_BX8_" + csclabel[endcap][station][ring][0] + ".png")
+    else:
+        c.SaveAs(outputdirectory  + "comparison_lct_" + varstr + "_" + csclabel[endcap][station][ring][0] + ".png")
 
 
 def compareALCTs(collections, endcap, station, ring, variable):
 
     nCollections = len(collections)
     ymins, ymaxs = yRanges(nCollections)
-        
+
     var = cscalctdigi[variable][0]
     varstr = cscalctdigi[variable][1]
     varnbin = cscalctdigi[variable][2]
@@ -244,7 +208,7 @@ def compareALCTs(collections, endcap, station, ring, variable):
 
     def addCollection(collection, index):
         collection_substring = collection[len('CSCDetIdCSCALCTDigiMuonDigiCollection_'):] + "_" + varstr + "_" + csclabel[endcap][station][ring][0]
-        hist = TH1D(collection_substring,"CSCALCTDigi " + varstr + " " + 
+        hist = TH1D(collection_substring,"CSCALCTDigi " + varstr + " " +
                    csclabel[endcap][station][ring][1] + "; " + varstr + "; Entries",varnbin,varminbin,varmaxbin)
 
         if station==0 and ring==0:
@@ -253,13 +217,10 @@ def compareALCTs(collections, endcap, station, ring, variable):
         else:
             tree.Draw(collection + ".obj.data_.second." + var + ">>" + hist.GetName(),
                       collection + ".obj.data_.first.endcap() == %d && "%(endcap) +
-                      collection + ".obj.data_.first.station()==%d && "%(station) + 
+                      collection + ".obj.data_.first.station()==%d && "%(station) +
                       collection + ".obj.data_.first.ring()==%d"%(ring))
-            
+
         hist.SetLineColor(colors[index])
-        if varstr is 'bx':
-            if hist.GetEntries()!=0:
-                print '|', csclabel[endcap][station][ring][1], '|', hist.GetBinContent(6)/hist.GetEntries(), '|', hist.GetBinContent(7)/hist.GetEntries(), '|', hist.GetBinContent(8)/hist.GetEntries(), '|' 
         if index==0:
             hist.Draw()
         else:
@@ -277,7 +238,7 @@ def compareALCTs(collections, endcap, station, ring, variable):
         if index==0: hist.Draw()
         else:        hist.Draw("sames")
         stats.Draw("same")
-        
+
     ## add the histograms and the stat boxes
     hists = []
     stats = []
@@ -296,7 +257,7 @@ def compareCLCTs(collections, endcap, station, ring, variable):
 
     nCollections = len(collections)
     ymins, ymaxs = yRanges(nCollections)
-        
+
     var = cscclctdigi[variable][0]
     varstr = cscclctdigi[variable][1]
     varnbin = cscclctdigi[variable][2]
@@ -308,18 +269,11 @@ def compareCLCTs(collections, endcap, station, ring, variable):
 
     def addCollection(collection, index):
         collection_substring = collection[len('CSCDetIdCSCCLCTDigiMuonDigiCollection_'):] + "_" + varstr + "_" + csclabel[endcap][station][ring][0]
-        hist = TH1D(collection_substring,"CSCCLCTDigi " + varstr + " " + 
+        hist = TH1D(collection_substring,"CSCCLCTDigi " + varstr + " " +
                    csclabel[endcap][station][ring][1] + "; " + varstr + "; Entries",varnbin,varminbin,varmaxbin)
 
         extraCut = "getKeyStrip() >=0"
         realRing = ring
-
-        if station==1 and ring==1:
-            extraCut = "getKeyStrip() <128"
-
-        if station==1 and ring==4:
-            realRing = 1
-            extraCut = "getKeyStrip() >=128"
 
         if station==0 and realRing==0:
             tree.Draw(collection + ".obj.data_.second." + var + ">>" + hist.GetName(),
@@ -327,14 +281,11 @@ def compareCLCTs(collections, endcap, station, ring, variable):
         else:
             tree.Draw(collection + ".obj.data_.second." + var + ">>" + hist.GetName(),
                       collection + ".obj.data_.first.endcap() == %d && "%(endcap) +
-                      collection + ".obj.data_.first.station()==%d && "%(station) + 
+                      collection + ".obj.data_.first.station()==%d && "%(station) +
                       collection + ".obj.data_.first.ring()==%d && "%(realRing) +
                       collection + ".obj.data_.second." + extraCut)
-            
+
         hist.SetLineColor(colors[index])
-        if varstr is 'bx':
-            if hist.GetEntries()!=0:
-                print '|', csclabel[endcap][station][ring][1], '|', hist.GetBinContent(6)/hist.GetEntries(), '|', hist.GetBinContent(7)/hist.GetEntries(), '|', hist.GetBinContent(8)/hist.GetEntries(), '|' 
         if index==0:
             hist.Draw()
         else:
@@ -352,7 +303,7 @@ def compareCLCTs(collections, endcap, station, ring, variable):
         if index==0: hist.Draw()
         else:        hist.Draw("sames")
         stats.Draw("same")
-        
+
     ## add the histograms and the stat boxes
     hists = []
     stats = []
@@ -368,52 +319,48 @@ def compareCLCTs(collections, endcap, station, ring, variable):
 
 
 def compareLCTsAll(collections):
-    for i in range(7,8):
+    for i in range(0,12):
         for p in cscstations:
-            compareLCTs(collections,1,p[0],p[1],i)
+            compareLCTs(collections,1,p[0],p[1],i,-1)
         for p in cscstations:
-            compareLCTs(collections,2,p[0],p[1],i)
+            compareLCTs(collections,2,p[0],p[1],i,-1)
+        for p in cscstations:
+            compareLCTs(collections,1,p[0],p[1],i,8)
+        for p in cscstations:
+            compareLCTs(collections,2,p[0],p[1],i,8)
 
 def compareALCTsAll(collections):
-    for i in range(5,6):
+    for i in range(0,6):
         for p in cscstations:
             if p[0]==1 and p[1]==4 : continue
             compareALCTs(collections,1,p[0],p[1],i)
         for p in cscstations:
             if p[0]==1 and p[1]==4 : continue
             compareALCTs(collections,2,p[0],p[1],i)
-        
+
 def compareCLCTsAll(collections):
-    for i in range(7,8):
+    for i in range(0,9):
         for p in cscstations:
             compareCLCTs(collections,1,p[0],p[1],i)
         for p in cscstations:
             compareCLCTs(collections,2,p[0],p[1],i)
-        
+
 lct_collections = [
-    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__DIGI2RAW',
-#    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis_MPCSORTED_DIGI2RAW',
-#    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_unpackCSC_MuonCSCCorrelatedLCTDigi_reL1T',
-#    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__reL1T',
-#    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis_MPCSORTED_reL1T',
-#    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_csctfUnpacker__reL1T',
-#    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_muonCSCDigis_MuonCSCCorrelatedLCTDigi_reL1T'
+    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis_MPCSORTED_ReL1',
+    'CSCDetIdCSCCorrelatedLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis_MPCSORTED_HLT',
 ]
 
 alct_collections = [
-    'CSCDetIdCSCALCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__DIGI2RAW',
-#    'CSCDetIdCSCALCTDigiMuonDigiCollection_unpackCSC_MuonCSCALCTDigi_reL1T',
-#    'CSCDetIdCSCALCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__reL1T',
-#    'CSCDetIdCSCALCTDigiMuonDigiCollection_muonCSCDigis_MuonCSCALCTDigi_reL1T'
+    'CSCDetIdCSCALCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__ReL1',
+    'CSCDetIdCSCALCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__HLT',
 ]
 
 clct_collections = [
-    'CSCDetIdCSCCLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__DIGI2RAW',
-#    'CSCDetIdCSCCLCTDigiMuonDigiCollection_unpackCSC_MuonCSCCLCTDigi_reL1T',
-#    'CSCDetIdCSCCLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__reL1T',
-#    'CSCDetIdCSCCLCTDigiMuonDigiCollection_muonCSCDigis_MuonCSCCLCTDigi_reL1T'
+    'CSCDetIdCSCCLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__ReL1',
+    'CSCDetIdCSCCLCTDigiMuonDigiCollection_simCscTriggerPrimitiveDigis__HLT',
 ]
- 
-#compareLCTsAll(lct_collections)
-#compareALCTsAll(alct_collections)
+
+compareLCTsAll(lct_collections)
+compareLCTsAll(lct_collections)
+compareALCTsAll(alct_collections)
 compareCLCTsAll(clct_collections)
