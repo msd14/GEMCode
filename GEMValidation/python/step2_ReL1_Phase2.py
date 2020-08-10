402e3219d5786ec8e5c5a65ee8ff3124c28ab9a1
Sven Dildick <sven.dildick@cern.ch>
2020-08-10 18:44:07 -0500

    CCLUT for other stations

--- GEMValidation/python/step2_ReL1_Phase2.py
+++ GEMValidation/python/step2_ReL1_Phase2.py
@@ -103,11 +103,15 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T15', ''
 process.simCscTriggerPrimitiveDigisRun3 = process.simCscTriggerPrimitiveDigis.clone()
 process.simCscTriggerPrimitiveDigisRun3.clctParam07.useRun3Patterns = cms.bool(True)
 process.simCscTriggerPrimitiveDigisRun3.clctSLHC.useRun3Patterns = cms.bool(True)
+process.simCscTriggerPrimitiveDigisRun3.clctSLHCME21.useRun3Patterns = cms.bool(True)
+process.simCscTriggerPrimitiveDigisRun3.clctSLHCME3141.useRun3Patterns = cms.bool(True)
 
 ## Run-3 patterns with CCLUT
 process.simCscTriggerPrimitiveDigisRun3CCLUT = process.simCscTriggerPrimitiveDigisRun3.clone()
 process.simCscTriggerPrimitiveDigisRun3CCLUT.clctParam07.useComparatorCodes = cms.bool(True)
 process.simCscTriggerPrimitiveDigisRun3CCLUT.clctSLHC.useComparatorCodes = cms.bool(True)
+process.simCscTriggerPrimitiveDigisRun3CCLUT.clctSLHCME21.useComparatorCodes = cms.bool(True)
+process.simCscTriggerPrimitiveDigisRun3CCLUT.clctSLHCME3141.useComparatorCodes = cms.bool(True)
 
 process.simEmtfDigis.CSCInput  = cms.InputTag('simCscTriggerPrimitiveDigis','MPCSORTED',"ReL1")
 
