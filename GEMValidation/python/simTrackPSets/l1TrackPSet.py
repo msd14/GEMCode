92fd91b8d6df2f984607c3c3eb399328d92f301e
Sven Dildick <sven.dildick@cern.ch>
2020-09-02 10:55:01 -0500

    TkMuon matcher

--- GEMValidation/python/simTrackPSets/l1TrackPSet.py
+++ GEMValidation/python/simTrackPSets/l1TrackPSet.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 l1TrackPSet = cms.PSet(
     l1Track = cms.PSet(
         verbose = cms.int32(1),
-        run = cms.bool(True),
+        run = cms.bool(False),
         minBX = cms.int32(0),
         maxBX = cms.int32(0),
         inputTag = cms.InputTag("TTTracksFromTrackletEmulation","Level1TTTracks"),
@@ -11,7 +11,7 @@ l1TrackPSet = cms.PSet(
     ),
     l1TkMuon = cms.PSet(
         verbose = cms.int32(1),
-        run = cms.bool(True),
+        run = cms.bool(False),
         inputTag = cms.InputTag("L1TkMuons"),
         minBX = cms.int32(0),
         maxBX = cms.int32(0),
