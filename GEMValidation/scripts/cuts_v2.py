4fde5a074f067b026440bcb555882bb5b7fd5068
Sven Dildick <sven.dildick@cern.ch>
2020-10-13 12:48:47 -0500

    stuff for rates

--- GEMValidation/scripts/cuts_v2.py
+++ GEMValidation/scripts/cuts_v2.py
@@ -1,4 +1,4 @@
-from ROOT import TH1F, TEfficiency, SetOwnership
+from ROOT import TH1F, TEfficiency, SetOwnership, kBlue
 import numpy as np
 import awkward1 as ak
 
@@ -46,56 +46,3 @@ def efficiency(denominator, num_cut, bins):
     numerator = denominator * num_cut
     th1_denom = TH1F("denom","denom", bins, np.array(denominator))
     th1_num = TH1F("num","num", bins, np.array(numerator))
-
-#_______________________________________________________________________________
-def draw_geff(t, title, h_bins, to_draw, den_cut, extra_num_cut,
-              opt = "", color = kBlue, marker_st = 1, marker_sz = 1.):
-    """Make an efficiency plot"""
-
-    ## total numerator selection cut
-    num_cut = AND(den_cut,extra_num_cut)
-
-    ## PyROOT works a little different than ROOT when you are plotting
-    ## histograms directly from tree. Hence, this work-around
-    nBins  = int(h_bins[1:-1].split(',')[0])
-    minBin = float(h_bins[1:-1].split(',')[1])
-    maxBin = float(h_bins[1:-1].split(',')[2])
-
-    num = TH1F("num", "", nBins, minBin, maxBin)
-    den = TH1F("den", "", nBins, minBin, maxBin)
-
-    t.Draw(to_draw + ">>num", num_cut, "goff")
-    t.Draw(to_draw + ">>den", den_cut, "goff")
-
-    debug = False
-    if debug:
-        print "Denominator cut", den_cut, den.GetEntries()
-        print "Numerator cut", num_cut, num.GetEntries()
-
-    ## check if the number of passed entries larger than total entries
-    doConsistencyCheck = False
-    if doConsistencyCheck:
-        for i in range(0,nBins):
-            print i, num.GetBinContent(i), den.GetBinContent(i)
-            if num.GetBinContent(i) > den.GetBinContent(i):
-                print ">>>Error: passed entries > total entries"
-
-    eff = TEfficiency(num, den)
-
-    ## plotting options
-    if not "same" in opt:
-        num.Reset()
-        num.GetYaxis().SetRangeUser(0.0,1.1)
-        num.SetStats(0)
-        num.SetTitle(title)
-        num.Draw()
-
-    eff.SetLineWidth(2)
-    eff.SetLineColor(color)
-    eff.SetMarkerStyle(marker_st)
-    eff.SetMarkerColor(color)
-    eff.SetMarkerSize(marker_sz)
-    eff.Draw(opt + " same")
-
-    SetOwnership(eff, False)
-    return eff
--- GEMValidation/scripts/flatPlots.py
+++ GEMValidation/scripts/flatPlots.py
@@ -11,19 +11,18 @@ sim_charge = events.array("sim_charge").flatten()
 sim_has_gem_pad = has_gem_pad(events)
 sim_has_gem_csc = has_csc_lct(events)
 
-print(sim_has_gem_pad)
-print(sim_has_gem_csc)
-print(sim_has_gem_csc * sim_has_gem_pad)
-print(sim_pt * sim_has_gem_csc * sim_has_gem_pad)
-print(sim_pt)
+## stuff for rates
+csc_clct_bx = events.array("csc_clct_bx").flatten()
+csc_alct_bx = events.array("csc_alct_bx").flatten()
+csc_clct_bx_me11 = events.array("csc_clct_bx")
 
+#print(len(csc_clct_bx[csc_clct_bx == 6csc_clct_bx == 7]))
+print(len(csc_clct_bx[csc_clct_bx == 6]))
+print(len(csc_clct_bx[csc_clct_bx == 7]))
+print(len(csc_clct_bx[csc_clct_bx == 8]))
+print(len(csc_clct_bx))
 
-#print(data)
-'''
-```
-In [47]: ak.num(has_gem1) > 0
-Out[47]: <Array [True, True, True, ... False, False] type='1722 * bool'>
-
-In [48]: has_gem1 = ak.flatten(has_gem, axis=1)
-``
-'''
+print(len(csc_alct_bx[csc_alct_bx == 2]))
+print(len(csc_alct_bx[csc_alct_bx == 3]))
+print(len(csc_alct_bx[csc_alct_bx == 4]))
+print(len(csc_alct_bx))
