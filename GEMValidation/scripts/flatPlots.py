171ea4e1c6aae1b4f28fcc21f5821ce6fe9e0385
Sven Dildick <sven.dildick@cern.ch>
2020-10-15 01:05:46 -0500

    Rates per station

--- GEMValidation/scripts/flatPlots.py
+++ GEMValidation/scripts/flatPlots.py
@@ -1,28 +1,41 @@
-import uproot
+import awkward1 as ak
+import uproot4
 import matplotlib.pyplot as plt
 from cuts_v2 import *
+from objects import getObjects, id_to_station
 
-file = uproot.open("out_ana_phase2.ntuple.root")
-print(file.keys())
+mycache = {}
 
-events = file["MuonNtuplizer"]["FlatTree"]
-sim_pt = events.array("sim_pt").flatten()
-sim_charge = events.array("sim_charge").flatten()
-sim_has_gem_pad = has_gem_pad(events)
-sim_has_gem_csc = has_csc_lct(events)
+tree = uproot4.open("out_ana_phase2.ntuple.root")["MuonNtuplizer"]["FlatTree"].arrays(array_cache=mycache)
 
-## stuff for rates
-csc_clct_bx = events.array("csc_clct_bx").flatten()
-csc_alct_bx = events.array("csc_alct_bx").flatten()
-csc_clct_bx_me11 = events.array("csc_clct_bx")
+csc_clct, csc_alct, csc_lct = getObjects(tree)
 
-#print(len(csc_clct_bx[csc_clct_bx == 6csc_clct_bx == 7]))
-print(len(csc_clct_bx[csc_clct_bx == 6]))
-print(len(csc_clct_bx[csc_clct_bx == 7]))
-print(len(csc_clct_bx[csc_clct_bx == 8]))
-print(len(csc_clct_bx))
+## rates for each station
+for ids in range(0,9):
+    station = id_to_station[ids][0]
+    ring = id_to_station[ids][1]
+    cuts = ((csc_alct.bx==3) &
+            (csc_alct.station == station) &
+            (csc_alct.ring == ring))
+    ak.to_list(cuts)
+    print("number of csc_alct for CSC station = %d, ring = %d and bunch crossing is 3 is :"%(station, ring), ak.sum(cuts))
 
-print(len(csc_alct_bx[csc_alct_bx == 2]))
-print(len(csc_alct_bx[csc_alct_bx == 3]))
-print(len(csc_alct_bx[csc_alct_bx == 4]))
-print(len(csc_alct_bx))
+
+for ids in range(0,9):
+    station = id_to_station[ids][0]
+    ring = id_to_station[ids][1]
+    cuts = ((csc_clct.bx==7) &
+            (csc_clct.station == station) &
+            (csc_clct.ring == ring))
+    ak.to_list(cuts)
+    print("number of csc_clct for CSC station = %d, ring = %d and bunch crossing is 6 is :"%(station, ring), ak.sum(cuts))
+
+
+for ids in range(0,9):
+    station = id_to_station[ids][0]
+    ring = id_to_station[ids][1]
+    cuts = ((csc_lct.bx==8) &
+            (csc_lct.station == station) &
+            (csc_lct.ring == ring))
+    ak.to_list(cuts)
+    print("number of csc_lct for CSC station = %d, ring = %d and bunch crossing is 6 is :"%(station, ring), ak.sum(cuts))
