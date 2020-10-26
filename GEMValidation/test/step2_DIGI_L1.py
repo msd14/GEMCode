310543ec84fa2031398a6b9640bb73b4fa9f1525
Matthew <msd14@rice.edu>
2020-10-26 17:01:57 -0500

    Added module for Run3CCLUT in CMSSW config.

---
+++ GEMValidation/test/step2_DIGI_L1.py
@@ -0,0 +1,167 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step2.py --filein file:step1.root --fileout file:step2.root --mc --eventcontent FEVTDEBUG --datatier GEN-SIM-DIGI-L1 --conditions auto:phase1_2021_realistic --step DIGI:pdigi_valid,L1 --geometry DB:Extended --era Run3 --python_filename step2_DIGI_L1.py --no_exec -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_cff import Run3
+
+process = cms.Process('ReL1',Run3)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(300),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+    #fileNames = cms.untracked.vstring('file:step1.root'),
+    fileNames = cms.untracked.vstring('/store/user/mdecaro/SingleMu_Run3_Pt1to10OneOverPt_noPU/CRAB3_SingleMu_Run3_Pt1to10OneOverPt_noPU/201019_200647/0000/step1_24.root'),
+    inputCommands = cms.untracked.vstring(
+        'keep *', 
+        ## raw
+        'drop *_rawDataCollector_*_*',
+        ## calorimeter
+        'drop *_simHGCal*_*_*',
+        'drop *_simHcal*_*_*',
+        'drop *_simEcal*_*_*',
+        'drop *_g4SimHits_Ecal*_*',
+        'drop *_g4SimHits_Hcal*_*',
+        'drop *_g4SimHits_HGC*_*',
+        'drop *_simCalo*_*_*',
+        'drop PCaloHits_*_*_*',
+         ## tracker
+        #'drop *_g4SimHits_Tracker*_*',
+        'drop *_simSiStrip*_*_*',
+        'drop *_simSiDigis*_*_*',
+        'drop *_simSiPixel*_*_*',
+        'drop *_TT*_*_*',
+        ## jets
+        'drop *_*GenJets*_*_*',
+        'drop *_*genMet*_*_*',
+        ## other
+        'drop PSimHits_*_Totem*_*',
+        'drop PSimHits_*_FP420*_*',
+        'drop PSimHits_*_BSC*_*',
+        'drop PSimHits_*_BSM*_*',
+        'drop PSimHits_g4SimHits_BHMHits_*',
+        'drop PSimHits_g4SimHits_PLTHits_*',
+        'drop *_mix_*_*',
+        'drop PSimHits_g4SimHits_FastTimerHits*_*',
+        'drop PSimHits_g4SimHits_CTPPS*_*',
+         ## barrel BMTF
+        'drop *_simBmtfDigis_*_*',
+        'drop *_simOmtfDigis_*_*',
+        'drop *_*_*BMTF*_*',
+        'drop *_*_*OMTF*_*',
+
+        'drop *_genParticles_*_*', 
+        'drop *_genParticlesForJets_*_*', 
+        'drop *_kt4GenJets_*_*', 
+        'drop *_kt6GenJets_*_*', 
+        'drop *_iterativeCone5GenJets_*_*', 
+        'drop *_ak4GenJets_*_*', 
+        'drop *_ak7GenJets_*_*', 
+        'drop *_ak8GenJets_*_*', 
+        'drop *_ak4GenJetsNoNu_*_*', 
+        'drop *_ak8GenJetsNoNu_*_*', 
+        'drop *_genCandidatesForMET_*_*', 
+        'drop *_genParticlesForMETAllVisible_*_*', 
+        'drop *_genMetCalo_*_*', 
+        'drop *_genMetCaloAndNonPrompt_*_*', 
+        'drop *_genMetTrue_*_*', 
+        'drop *_genMetIC5GenJs_*_*'
+    ),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(2),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step2.py nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-L1'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:step2.root'),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+
+from GEMCode.GEMValidation.cscTriggerCustoms import addCSCTriggerRun3
+process = addCSCTriggerRun3(process)
+
+# Additional output definition
+
+# Other statements
+process.mix.digitizers = cms.PSet(process.theDigitizersValid)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase1_2021_realistic', '')
+
+# Path and EndPath definitions
+process.digitisation_step = cms.Path(process.pdigi_valid)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
